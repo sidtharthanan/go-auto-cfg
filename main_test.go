8cffceacf25e8ebc2c390b7dd94b3ca15d2154cb
Sidtharthan_A_N <sidthunagarajan@gmail.com>
2018-02-06 22:13:04 +0530

    Load configs with file name and directory

--- main_test.go
+++ main_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestBasicDataTypes(t *testing.T) {
-	t1.Load("config1", ".", "test_samples")
+	t1.Load("config1", "test_samples")
 
 	assert.Equal(t, 50, t1.POOL_SIZE())
 	assert.Equal(t, []string{"staging", "production"}, t1.ENVS())
@@ -19,10 +19,10 @@ func TestBasicDataTypes(t *testing.T) {
 
 func TestMultipleInstances(t *testing.T) {
 	tInstance1 := t1.New()
-	tInstance1.Load("config1", ".", "test_samples")
+	tInstance1.Load("config1", "test_samples")
 
 	tInstance2 := t1.New()
-	tInstance2.Load("config1.2", ".", "test_samples")
+	tInstance2.Load("config1.2", "test_samples")
 
 	assert.Equal(t, "busyqueue", tInstance1.QUEUE_NAME())
 	assert.Equal(t, "freequeue", tInstance2.QUEUE_NAME())
